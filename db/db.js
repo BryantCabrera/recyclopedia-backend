69d1f398c7af7fc2144747f093091a2fa2c2b9b7
Bryant <cabrera.bryant@gmail.com>
2019-04-20 00:49:04 -0700

    Bryant sets up db.js.

---
+++ db/db.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const connectionString = process.env.DATABASE_URL;
+
+mongoose.connect(connectionString, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+});
+
+mongoose.connection.on('connected', () => {
+    console.log(`Mongoose connected to ${connectionString}.`);
+});
+
+mongoose.connection.on('error', (err) => {
+    console.log(`Mongoose ran into an ${err}.`);
+});
+
+mongoose.connection.on('disconnected', () => {
+    console.log(`Mongoose disconnected from ${connectionString}.`);
+});
